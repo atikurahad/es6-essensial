598dc161f17f720e473edf5104d21b1acbda9abd
atikurahad <atikurahad@gmail.com>
2024-01-21 13:13:20 +0600

    loop hoisting object

--- ecmaScripit.js
+++ ecmaScripit.js
@@ -17,9 +17,52 @@
 
 //-------------------------
 
-//Dynamic Function 
+//Dynamic Function
 
-const name = function (userName){
-    return userName;
-}
-console.log(name("I am Dev"));
+// const name = function (userName){
+//     return userName;
+// }
+// console.log(name("I am Dev"));
+
+//VARIABLE HOISTING
+//--------------
+// name = "abdul";
+// console.log(name)
+// var name;
+
+// Simple for lopp
+// let i =0;
+// for (let i = 0; i <= 500; i++) {
+//     console.log(`${i} `)
+
+// }
+
+// FOR OF LOOP_____________
+
+// let city = ['Dhaka','Rajshahi','Barishal','Tangail','Bogra'];
+
+// for (const myCity of city) {
+//     console.log(myCity)
+// }
+
+//OBJECT-----------
+
+// let person ={name:"bill",eye:"yes",nose:"yes",hair:"black"};
+
+//Nested Object
+
+// let person = {
+//   shirt: {
+//     color: "black",
+//     brand: "eleven",
+//   },
+//   dress: {
+//     color: "White",
+//     type: "cotton",
+//   },
+//   hair: {
+//     color: "white",
+//     size: "short",
+//   },
+// };
+// console.log(person.dress.color);
